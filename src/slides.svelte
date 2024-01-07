c2dffaa63121e417463e60f11723245f4da1eeb9
AnranW <echowang1996@hotmail.com>
2024-01-07 12:28:55 +0100

    layout again

--- src/slides.svelte
+++ src/slides.svelte
@@ -32,7 +32,7 @@
 	<Slide animate id="charge">
 		<Layout>
 			<a href="#main">
-				<img class="h-full inline" src="https://i.pinimg.com/originals/db/49/58/db4958c8fab9519e8e8e3562b35c6757.gif" alt="charging">
+				<img  class="w-full inline center mt-[15vh]" src="https://i.pinimg.com/originals/db/49/58/db4958c8fab9519e8e8e3562b35c6757.gif" alt="charging">
 			</a>
 		</Layout>
 	</Slide>
@@ -40,7 +40,7 @@
 	<Slide animate id="play">
 		<Layout> 
 			<a href="#main">
-				<img class="inline" style="height:60vh; padding-bottom:3vh; padding-bottom:2vh;" src="https://media1.giphy.com/media/jUJtNKKVTdIeD0XreN/giphy.gif?cid=6c09b952vj867wh6lh9pkhay7tgurq6tmi07zyhraij65eoi&ep=v1_stickers_related&rid=giphy.gif&ct=s" alt="playing">
+				<img class="inline" style="width:45vh; padding-top:10vh;" src="https://media1.giphy.com/media/jUJtNKKVTdIeD0XreN/giphy.gif?cid=6c09b952vj867wh6lh9pkhay7tgurq6tmi07zyhraij65eoi&ep=v1_stickers_related&rid=giphy.gif&ct=s" alt="playing">
 			</a>
 		</Layout>
 	</Slide>
@@ -48,11 +48,11 @@
 	<Slide animate id="diagnose">
 		<Layout> 
 			<br>
-			<img class="h-[40vh] float-left" src="https://www.svgrepo.com/show/503282/diagnostic-alt.svg" alt="diagnosing">
+			<img class="h-[40vh] float-top ml-auto mr-auto" src="https://www.svgrepo.com/show/503282/diagnostic-alt.svg" alt="diagnosing">
 			<Stack>
 				<Step fadeOut> 
 					<br>
-					<img class="h-[20vh]" src="https://i.gifer.com/CVyf.gif" alt="diagnosing">			
+					<img class="h-[10vh]" src="https://i.gifer.com/CVyf.gif" alt="diagnosing">			
 				</Step>			
 				<Step fadeIn>
 					<br>
